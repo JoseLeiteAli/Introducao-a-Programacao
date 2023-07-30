4aa3c16328b7f24d08a0f5f930d531dd35a8b08e
JoseLeiteAli <137967622+JoseLeiteAli@users.noreply.github.com>
2023-07-30 20:40:02 -0300

    Add files via upload

---
+++ Projeto - Calculadora.py
@@ -0,0 +1,26 @@
+#Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da operação e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
+#1. Soma
+#2. Subtração
+#3. Multiplicação
+#4. Divisão
+#Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
+
+def calculadora(numero1, operador, numero2):
+    if operador == "+" : return numero1 + numero2
+
+    elif operador == "-": return numero1 - numero2
+
+    elif operador == "*": return numero1 * numero2
+
+    elif operador == "/": return numero1 / numero2
+
+    else : return 0
+
+numero1=int(input("Digite um número: "))
+
+operador=str(input("Digite um operador: "))
+
+numero2=int(input("Digite um número: "))
+
+resultado=(calculadora(numero1, operador, numero2))
+print(resultado)
